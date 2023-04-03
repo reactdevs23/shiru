50c16a02beca6bcb1d9e18d02a0afd7d8cb5469d
dalimkumar604 <dalimkumar212582@gmail.com>
2023-04-03 21:45:43 +0600

    adkasd

--- src/components/Partners/Partners.js
+++ src/components/Partners/Partners.js
@@ -1,28 +1,36 @@
 import React from "react";
+import {
+  invezz,
+  crypto,
+  blockonomy,
+  tradingview,
+  bloomberg,
+  zycrypto,
+} from "../../images/images";
 import styles from "./Partners.module.css";
 
 function App() {
-  const row1 = [
-    "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/7ae42bac3b34999c0db3.png",
-    "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/b2bd91d7b87b2181ca45.png",
-    "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/6591cdc0702b32310306.png",
-    "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/3b7d9f4b073deb6a9b74.png",
-    "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/b2bd91d7b87b2181ca45.png",
-    "https://assets.algoexpert.io/spas/main/prod/g523bdeb478-prod/dist/images/a2b3c3709ffedce2a22a.png",
+  const partners = [
+    invezz,
+    crypto,
+    blockonomy,
+    tradingview,
+    bloomberg,
+    zycrypto,
   ];
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.marquee}>
         <div className={styles.marqueGroup}>
-          {row1.map((el, i) => (
+          {partners.map((el, i) => (
             <div className={styles.imageGroup} key={i}>
               <img src={el} alt="#" className={styles.image} />
             </div>
           ))}
         </div>
         <div className={styles.marqueGroup}>
-          {row1.map((el, i) => (
+          {partners.map((el, i) => (
             <div className={styles.imageGroup} key={i}>
               <img src={el} alt="#" className={styles.image} />
             </div>
--- src/images/images.js
+++ src/images/images.js
@@ -1,5 +1,21 @@
 import progressbarIcon from "./progressbarIcon.svg";
 import copy from "./copy.svg";
 import logo from "./logo.svg";
+import invezz from "./invezz.png";
+import crypto from "./crypto.png";
+import blockonomy from "./blockonomi.png";
+import tradingview from "./tradingview.png";
+import bloomberg from "./bloomberg.png";
+import zycrypto from "./zycrypto.png";
 
-export { progressbarIcon, copy, logo };
+export {
+  progressbarIcon,
+  copy,
+  logo,
+  invezz,
+  crypto,
+  blockonomy,
+  tradingview,
+  bloomberg,
+  zycrypto,
+};
